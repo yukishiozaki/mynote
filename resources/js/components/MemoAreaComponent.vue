8c5011d0543ee6772048925a83c565cb28c09712
h-inouchi <hibird2720804@gmail.com>
2019-11-11 16:46:33 +0900

    memo area vue component add

---
+++ resources/js/components/MemoAreaComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <form action="/notes/edit" method="post" enctype="multipart/form-data">
+    <textarea class="memo-area" rows="5">{{ contents }}</textarea>
+  </form>
+</template>
+
+<script>
+  export default {
+    props: ['contents'],
+    mounted() {
+        console.log('Component mounted inouchi.');
+    }
+  }
+</script>
